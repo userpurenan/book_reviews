da05e99c8b88ab3b2ee48b19974ef0ce5a578dd1
userpurenan <nan691628@gmail.com>
2023-10-11 07:58:56 +0900

    update

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('token', 220)->unique();
             $table->timestamps();
         });
     }
