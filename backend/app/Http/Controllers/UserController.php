ffd8af561c9b63da35b957319d8ad0416ac2e3a1
userpurenan <nan691628@gmail.com>
2023-10-15 00:01:01 +0900

    update

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -5,39 +5,84 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\User;
 
 class UserController extends Controller
 {
     public function signUp(Request $request){
-        $name = $request->input('name');
-        $email = $request->input('email');
-        $password = $request->input('password');
-        $access_token = Str::random(220);
+        DB::beginTransaction();
 
-        User::insert([
-            "name" => $name,
-            "email" => $email,
-            "password" => Hash::make($password),
-            "token" => $access_token,
-        ]);
-        
-        return response()->json([ 'token' => $access_token ], 200);
+        try{
+            User::insert([
+                "name" => $request->input('name'),
+                "email" => $request->input('email'),
+                "password" => Hash::make($request->input('password')),
+            ]);
+
+            $credentials = $request->only(['email', 'password']);
+            if(! $token = auth()->attempt($credentials)) throw new \Exception('トークンの取得に失敗しました。') ;
+
+            User::where('email', $request->input('email'))->update(['token' => 'Bearer '.$token]);
+
+            DB::commit();
+            
+            return response()->json([ 
+                    'token' => $token,
+                    'token_type' => 'bearer',
+                    'expires_in' => auth("api")->factory()->getTTL() * 60
+                ], 200);    
+        }catch(\Exception $e){
+            DB::rollBack();
+            return response()->json(['errormessage' => $e ]);
+        }
     }
 
     public function login(Request $request){
         try{
-            $access_token = Str::random(220);
             $credentials = $request->only('email', 'password');
-            $guard = $request->guard;
 
-            if (!Auth::guard($guard)->attempt($credentials)) throw new \Exception('パスワード又はメールアドレスが間違っています');
+            if (! $token = auth()->attempt($credentials)) throw new \Exception('パスワード又はメールアドレスが間違っています');
 
-            return response()->json([ 'token' => $access_token ], 200);
+            return response()->json([ 
+                    'token' => $token,
+                    'token_type' => 'bearer',
+                    'expires_in' => auth("api")->factory()->getTTL() * 60
+                ], 200);
         }catch(\Exception $e){
             return response()->json(['error' => $e], 500);        
         }
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        return response()->json(['message' => $request->input('success')]);
+    }
+
+    public function imgUpdate(Request $request){
+        $imgPath = $request->file('icon')->store('public/img');
+
+        User::where('token', $request->headers->get('Authorization'))
+                ->update(['imgPath' => 'backend/storage/app/'.$imgPath]);
+        
+        return response()->json(['imgPath' => "backend/storage/app/".$imgPath]);
+    }
+
+    public function getUser(Request $request){
+        $user = User::where('token', $request->headers->get('Authorization'))->first();
+
+        return response()->json([ 
+                    'name' => $user->name,
+                    'imgPath' => $user->imgPath,
+                ], 200);
+    }
+
+    public function editUser(Request $request){
+        User::where('token', $request->headers->get('Authorization'))
+                ->update(['name' => $request->input('name')]);
     }
 
 }
