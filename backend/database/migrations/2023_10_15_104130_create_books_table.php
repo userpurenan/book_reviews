c65d5669e15597c6253c43d43f3a97f467614cba
userpurenan <nan691628@gmail.com>
2023-10-17 21:57:49 +0900

    update

---
+++ backend/database/migrations/2023_10_15_104130_create_books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('url');
+            $table->text('detail');
+            $table->text('review');
+            $table->text('reviewer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book');
+    }
+};
