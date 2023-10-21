6c99bd1b0708969994981c8a6a516ab663904a73
userpurenan <nan691628@gmail.com>
2023-10-21 16:33:47 +0900

    ダミーデータ項目追加

--- backend/database/factories/BookFactory.php
+++ backend/database/factories/BookFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
@@ -21,7 +22,8 @@ class BookFactory extends Factory
             'url' => "test@test.com",
             'detail' => "テストです",
             'review' => "テスト",
-            'reviewer' => "きよみや"
+            'reviewer' => "きよみや",
+            'token' => "Bearer ".Str::random(220)
         ];
     }
 }
