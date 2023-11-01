a1ef021b65eb9bff5356b78b513e7e56c394d4ac
userpurenan <nan691628@gmail.com>
2023-11-01 22:17:58 +0900

    細かいところ修正

--- backend/app/Models/Log.php
+++ backend/app/Models/Log.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use backend\App\Models\User;
 
 class Log extends Model
 {
@@ -18,4 +19,7 @@ class Log extends Model
 
     protected $guarded = ['created_at', 'updated_at'];
 
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- backend/database/migrations/2023_10_22_060430_create_logs_table.php
+++ backend/database/migrations/2023_10_22_060430_create_logs_table.php
@@ -13,8 +13,6 @@ return new class extends Migration
     {
         Schema::create('logs', function (Blueprint $table) {
             $table->id();
-            // $table->text('user_name');
-            // $table->text('user_email');
             $table->unsignedBigInteger('user_id');
             $table->text('access_log');
             $table->timestamps();
