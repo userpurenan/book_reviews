5b67f88511e4f53aa55e14d83f12ac8911cf4191
userpurenan <nan691628@gmail.com>
2023-12-10 21:57:42 +0900

    認証実装

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -5,14 +5,13 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
 
-use Illuminate\Http\Request;
 use App\Http\Requests\SignUpRequest;
 use App\Http\Requests\LoginRequest;
 use App\Models\User;
@@ -22,37 +21,51 @@ class UserController extends Controller
 {
     public function signUp(SignUpRequest $request)
     {
-        DB::transaction(function () use ($request, &$user, &$token){
+        DB::transaction(function () use ($request, &$user){
             $user = User::create([
                 "name" => $request->input('name'),
                 "email" => $request->input('email'),
                 "password" => Hash::make($request->input('password')),
             ]);
-
-            $credentials = $request->only(['email', 'password']);
-            if(! $token = Auth::attempt($credentials)) throw new \Exception('トークンの取得に失敗しました。') ;
-    
-            Token::create(['user_id' => $user->id,
-                               'token' => $token]);    
         });
 
-        return response()->json([ 'name' => $user->name, 'token' => $token ],200, [], JSON_UNESCAPED_UNICODE)->header('Authorization', 'Bearer '.$token);
+        $data = [
+            'grant_type' => 'password',
+            'client_id' => '2',
+            'client_secret' => '9vmWqyeJgJnxKcR6cDj11cjj77ON32NB9x1IN3gu',
+            'username' => $request->input('email'),
+            'password' => $request->input('password'),
+            'scope' => '',
+        ];
+
+        $request = Request::create('/oauth/token', 'POST', $data);
+        $response = Route::prepareResponse($request, app()->handle($request));
+
+        $content = $response->getContent();
+
+        $token = json_decode($content, true);
+
+        return response()->json([ 'name' => $user->name, 'token' => $token['access_token'] ],200, [], JSON_UNESCAPED_UNICODE);
     }
 
     public function login(LoginRequest $request)
     {
-        $credentials = $request->only('email', 'password');
+        $data = [
+                    'grant_type' => 'password',
+                    'client_id' => '2',
+                    'client_secret' => '9vmWqyeJgJnxKcR6cDj11cjj77ON32NB9x1IN3gu',
+                    'username' => $request->input('email'),
+                    'password' => $request->input('password'),
+                    'scope' => '',
+                ];
 
-        if(! $token = Auth::attempt($credentials)) {
-            throw new BadRequestHttpException('パスワード又はメールアドレスが間違っています');
-        }
+        $request = Request::create('/oauth/token', 'POST', $data);
+        $response = Route::prepareResponse($request, app()->handle($request));
+        $content = $response->getContent();
 
-        $user = User::where('email', $request->input('email'))->first();
+        $token = json_decode($content, true);
 
-        Token::create(['user_id' => $user->id,
-                       'token' => $token]);
-
-        return response()->json([ 'message' => 'auth success', 'token' => $token ], 200)->header('Authorization', 'Bearer '.$token);
+        return response()->json(['access_token' => $token['access_token']]);
     }
 
     public function imageUploads(Request $request)
