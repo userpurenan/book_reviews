6ca62554264798f9cc86b2a3a504949690e86a9a
userpurenan <nan691628@gmail.com>
2023-12-17 22:39:02 +0900

    コメント機能のフロント部分

--- src/book_review/book_review_detail/BookReviewDetail.jsx
+++ src/book_review/book_review_detail/BookReviewDetail.jsx
@@ -3,7 +3,7 @@ import { useCookies } from 'react-cookie';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { url } from '../../const';
-import { ReviewComment } from '../../review_comment/ReviewComment';
+import { ReviewCommentInput } from '../review_comment/ReviewCommentInput';
 import Loading from '../Loding';
 import { Header } from '../header/Header';
 import './BookReviewDetail.scss';
@@ -14,6 +14,7 @@ export const BookReviewDetail = () => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   const [bookData, setBookData] = useState('');
+  const [BookComment, setBookComment] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
   const headers = {
@@ -27,8 +28,8 @@ export const BookReviewDetail = () => {
 
     axios
       .get(`${url}/books/${BookId}`, { headers })
-      .then((res) => {
-        const bookData = res.data;
+      .then((response) => {
+        const bookData = response.data;
         setBookData(bookData); //書籍の情報を一個にまとめた
       })
       .catch((err) => {
@@ -39,13 +40,19 @@ export const BookReviewDetail = () => {
         setIsLoading(false);
       });
 
-    axios.post(`${url}/logs`, { selectBookId: BookId }, { headers }).catch((err) => {
+    axios.post(`${url}/logs`, { selectBookId: BookId }, { headers })
+    .catch((err) => {
       setErrorMessage(`ログの送信に失敗しました${err}`);
     });
+
+    axios.get(`${url}/books/${BookId}/comment`, { headers })
+     .then((response) => {
+        setBookComment(response.data)
+     })
   }, [BookId]);
 
   return (
-    <div>
+    <div className='page'>
       <Header />
       <h1>書籍の詳細</h1>
       <h2 className="error-massage">{errorMessage}</h2>
@@ -54,14 +61,20 @@ export const BookReviewDetail = () => {
       ) : (
         <div className="bookDetail">
           <p className="bookDetail__title">タイトル: {bookData.title}</p>
-          <p className="bookDetail__url">
-            URL: <a href={bookData.url}>{bookData.url}</a>
-          </p>
+          <p className="bookDetail__url">URL: <a href={bookData.url}>{bookData.url}</a></p>
           <p className="bookDetail__reviewer">レビュワー: {bookData.reviewer}</p>
           <p className="bookDetail__detail">書籍の詳細情報: {bookData.detail}</p>
           <p className="bookDetail__review">レビュー: {bookData.review}</p>
-          {bookData.isMine ? <Link to={`/edit/${BookId}`}>書籍編集画面へ</Link> : <></>}
-          <ReviewComment />
+          {bookData.isMine ? <Link to={`/edit/${BookId}`} className='bookDetail__link-edit-book'>書籍編集画面へ</Link> : <></>}
+          <ReviewCommentInput />
+          <ul>
+            {BookComment.map((BookCommentList, key) => (
+              <li key={key} value={BookCommentList.id} className='comment_list'>
+                  {BookCommentList.user_name}<br />
+                  <p className='user_comment'>{BookCommentList.comment}</p>
+              </li>            
+            ))}
+          </ul>
         </div>
       )}
     </div>
