15aacaf3ff554eae6cae6becf01efeff3ceef692
userpurenan <nan691628@gmail.com>
2023-12-29 22:11:59 +0900

    書籍作成テスト追加

--- backend/tests/Unit/Book/BookTest.php
+++ backend/tests/Unit/Book/BookTest.php
@@ -3,50 +3,25 @@
 namespace Tests\Unit\Book;
 
 use App\Models\Book;
-use App\Models\User;
 use Faker\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class BookTest extends TestCase
 {
     use RefreshDatabase;
 
-    private $password;
-
-    private $email;
-
-    /**
-     * ユーザーを作成するメソッド。
-     */
-    public function create_user()
-    {
-        $faker = Factory::create('ja_JP');
-        $this->password = Str::random(10);
-        $this->email = $faker->safeEmail;
-
-        $user = User::create([
-                          'name' => '岩崎太郎',
-                          'email' => $this->email,
-                          'password' => Hash::make($this->password)
-                      ]);
-
-        return $user;
-    }
-
     public function createBook(int $record)
     {
-        $user = $this->create_user();
+        $user = $this->createUser();
         $faker = Factory::create('ja_JP');
         for($i=1; $i<=$record; $i++){
             Book::create([
                 'title' => 'ワンピース',
                 'user_id' => $user->id,
                 'url' => 'sample.com',
-                'detail' => $faker->realText($maxNbChars = 15),
-                'review' => $faker->realText($maxNbChars = 30),
+                'detail' => $faker->realText(15),
+                'review' => $faker->realText(30),
                 'reviewer' => $user->name,
             ]);
         }
@@ -55,8 +30,8 @@ class BookTest extends TestCase
                 'title' => 'NARUTO',
                 'user_id' => $user->id,
                 'url' => 'sample.com',
-                'detail' => $faker->realText($maxNbChars = 15),
-                'review' => $faker->realText($maxNbChars = 30),
+                'detail' => $faker->realText(15),
+                'review' => $faker->realText(30),
                 'reviewer' => $user->name,
             ]);
         }
@@ -65,8 +40,8 @@ class BookTest extends TestCase
     public function test_本を取得できるか？(): void
     {
         $this->createBook(100);
-        $books = $this->get('/api/public/books');
-        $books->assertStatus(200);
+        $books = $this->get('/api/public/books')->json();
+        $this->assertCount(10, $books);
     }
 
     public function test_検索処理を実行できるか？()
@@ -74,5 +49,29 @@ class BookTest extends TestCase
         $this->createBook(5000);
         $books = $this->get('/api/public/books?title_keyword=NARUTO');
         $books->assertJsonMissing(["title" => "ワンピース"]);
+    }
+
+    public function test_新規の書籍レビューを作れるか？()
+    {
+        $books = $this->get('/api/public/books')->json();
+        $this->assertCount(0, $books);
+
+        $user = $this->createUser();
+        $token = $this->createToken($this->email, $this->password);
+        $faker = Factory::create('ja_JP');
+
+        $this->post('/api/books', [
+            'title' => $faker->realtext(10),
+            'user_id' => $user->id,
+            'url' => 'sample.com',
+            'detail' => $faker->realText(15),
+            'review' => $faker->realText(30),
+            'reviewer' => $user->name,
+        ],[
+            'Authorization' => 'Bearer '.$token,
+        ]);
+
+        $books = $this->get('/api/public/books')->json();
+        $this->assertCount(1, $books);
     }
 }
