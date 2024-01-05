eb62fae98585121c2325675d270fbf210b14f2d0
userpurenan <nan691628@gmail.com>
2024-01-05 14:21:28 +0900

    データプロバイダを使って書き直した

--- backend/tests/Feature/app/Http/Controllers/User/UserTest.php
+++ backend/tests/Feature/app/Http/Controllers/User/UserTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Unit\User;
+namespace Tests\Feature\App\Http\Controllers\User;
 
 use Faker\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -13,73 +13,41 @@ class UserTest extends TestCase
 
     public function test_会員登録できるか？(): void
     {
-        $this->createUser();
-        $this->assertDatabaseCount('users', 1);
-
         $faker = Factory::create('ja_JP');
         $response = $this->post('/api/signup',[
                                 'name' => $faker->name,
                                 'email' => $faker->safeEmail,
-                                'password' => $this->password
+                                'password' => Str::random(10)
                             ]);
 
         $response->assertStatus(200);
     }
 
-    public function test_会員登録の際にバリデーションルールが適応されているか？()
+    /**
+     * @dataProvider validationFailsProvider
+     */
+    public function test_会員登録時のバリデーションチェック($name, $email, $password)
+    {
+        $invalid = $this->post('/api/signup',[
+            'name' => $name,
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        $invalid->assertStatus(422);
+    }
+
+    public static function validationFailsProvider()
     {
         $faker = Factory::create('ja_JP');
-
-        //無効なメールアドレス形式
-        $invalid_email_adress = $this->post('/api/signup',[
-                                    'name' => $faker->name,
-                                    'email' => 'sample',
-                                    'password' => Str::random(10)
-                                ]);
-
-        $invalid_email_adress->assertStatus(422);
-
-        //無効なパスワード(4文字以下)
-        $invalid_password_length_max4 = $this->post('/api/signup',[
-                                            'name' => $faker->name,
-                                            'email' => $faker->safeEmail,
-                                            'password' => Str::random(rand(1,4))
-                                        ]);
-        
-        //無効なパスワード（16文字以上）
-        $invalid_password_length_min16 = $this->post('/api/signup',[
-                                            'name' => $faker->name,
-                                            'email' => $faker->safeEmail,
-                                            'password' => Str::random(rand(16, 17))
-                                        ]);
-        
-        $invalid_password_length_max4->assertStatus(422);
-        $invalid_password_length_min16->assertStatus(422);
-
-        //名前が入力されていない状態
-        $invalid_name_null = $this->post('/api/signup',[
-                                'name' => '',
-                                'email' => $faker->safeEmail,
-                                'password' => Str::random(10)
-                            ]);
-
-        //メールアドレスが入力されていない状態
-        $invalid_email_null = $this->post('/api/signup',[
-                                'name' => $faker->name,
-                                'email' => '',
-                                'password' => Str::random(rand(16, 17))
-                            ]);
-
-        //パスワードが入力されていない状態
-        $invalid_password_null = $this->post('/api/signup',[
-                                    'name' => $faker->name,
-                                    'email' => $faker->safeEmail,
-                                    'password' => ''
-                                ]);
-
-        $invalid_name_null->assertStatus(422);
-        $invalid_email_null->assertStatus(422);
-        $invalid_password_null->assertStatus(422);
+        return[
+            '無効なメールアドレス' => [ $faker->name, 'sample', Str::random(10) ],
+            '無効なパスワード（4文字以下）' => [ $faker->name, $faker->safeEmail, Str::random(rand(1,4)) ],
+            '無効なパスワード（16文字以下）' => [ $faker->name, $faker->safeEmail, Str::random(rand(16, 17)) ],
+            '名前が入力されていない状態' => [ '', $faker->safeEmail, Str::random(10) ],
+            'メールアドレスが入力されていない状態' => [ $faker->name, '', Str::random(10) ],
+            'パスワードが入力されていない状態' => [ $faker->name, $faker->safeEmail, '' ]
+        ];
     }
 
     public function test_ログインできるか？()
@@ -93,53 +61,16 @@ class UserTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_ログインの際にバリデーションルールが適応されているか？()
+    /**
+     * @dataProvider validationFailsProvider
+     */
+    public function test_ログインの際にバリデーションルールが適応されているか？($email, $password)
     {
-        $faker = Factory::create('ja_JP');
+        $invalid = $this->post('/api/login',[
+            'email' => $email,
+            'password' => $password
+        ]);
 
-        //無効なメールアドレス形式
-        $invalid_email_adress = $this->post('/api/login',[
-                                    'name' => $faker->name,
-                                    'email' => 'sample',
-                                    'password' => Str::random(10)
-                                ]);
-
-        $invalid_email_adress->assertStatus(422);
-
-        //無効なパスワード(4文字以下)
-        $invalid_password_length_max4 = $this->post('/api/login',[
-                                            'name' => $faker->name,
-                                            'email' => $faker->safeEmail,
-                                            'password' => Str::random(rand(1,4))
-                                        ]);
-        
-        //無効なパスワード（16文字以上）
-        $invalid_password_length_min16 = $this->post('/api/login',[
-                                            'name' => $faker->name,
-                                            'email' => $faker->safeEmail,
-                                            'password' => Str::random(rand(16, 17))
-                                        ]);
-        
-        $invalid_password_length_max4->assertStatus(422);
-        $invalid_password_length_min16->assertStatus(422);
-
-        //メールアドレスが入力されていない状態
-        $invalid_email_null = $this->post('/api/login',[
-                                'name' => $faker->name,
-                                'email' => '',
-                                'password' => Str::random(rand(16, 17))
-                            ]);
-
-        //パスワードが入力されていない状態
-        $invalid_password_null = $this->post('/api/login',[
-                                    'name' => $faker->name,
-                                    'email' => $faker->safeEmail,
-                                    'password' => ''
-                                ]);
-
-        $invalid_email_null->assertStatus(422);
-        $invalid_password_null->assertStatus(422);
+        $invalid->assertStatus(422);
     }
-
-
 }
