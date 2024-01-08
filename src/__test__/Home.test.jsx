befa3000bbc900176b1485ebd5bc876a0660acf4
userpurenan <nan691628@gmail.com>
2024-01-08 23:59:41 +0900

    update

--- src/__test__/Home.test.jsx
+++ src/__test__/Home.test.jsx
@@ -1,10 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import Home  from '../book_review/Home';
 
-describe("テスト", () => {
+describe("ホーム画面の要素があるか", () => {
+    beforeEach(() => { //「render」は各テスト毎に実行する必要があるから「beforEach」を使う
+        render(
+          <Provider store={store}>
+            <CookiesProvider>
+              <BrowserRouter>
+                <Home />
+              </BrowserRouter>      
+            </CookiesProvider>
+          </Provider>
+          );  
+      });
+
     test('見出しがあるか', () => {
-        render(<Home />);
         const hometitle = screen.getByText(/書籍レビュー/);
         expect(hometitle).toBeInTheDocument();
-    })
+    });
+
+    test('')
 })
