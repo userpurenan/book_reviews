68bb4caf9479cea45560572efdd18e467eb1d267
userpurenan <nan691628@gmail.com>
2024-01-13 23:07:08 +0900

    トークンが無い状態でトークンが必要なページにアクセスした場合にエラーページを表示　フロントエンド

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { BookReviewDetail } from '../book_review/book_review_detail/BookReviewDetail';
 import { CreateBookReview } from '../book_review/create_book_review/CreateBookReview';
 import { EditBookReview } from '../book_review/edit_book_review/EditBookReview';
 import { EditProfile } from '../book_review/edit_profile/EditProfile';
+import { ErrorPage } from '../book_review/error_page/ErrorPage';
 import { Home } from '../book_review/home/Home';
 import { Login } from '../book_review/login/Login';
 import { SignUp } from '../book_review/signup/SignUp';
 
 export const Router = () => {
-  //   const auth = use;
+  const auth = useSelector((state) => state.auth.isSignIn);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<Home />} />
-        <Route path="/edit/profile" element={<EditProfile />} />
-        <Route path="/new" element={<CreateBookReview />} />
-        <Route path="/detail/:BookId" element={<BookReviewDetail />} />
-        <Route path="/edit/:BookId" element={<EditBookReview />} />
+        {auth ? (
+          <>
+            <Route path="/edit/profile" element={<EditProfile />} />
+            <Route path="/new" element={<CreateBookReview />} />
+            <Route path="/detail/:BookId" element={<BookReviewDetail />} />
+            <Route path="/edit/:BookId" element={<EditBookReview />} />
+          </>
+        ) : (
+          <Route path="*" element={<ErrorPage />} />
+        )}
       </Routes>
     </BrowserRouter>
   );
