2ae5603f33c71c3a1aa0f2fb94baac58ebe48d98
userpurenan <nan691628@gmail.com>
2024-01-17 23:18:55 +0900

    update

--- src/authSlice.js
+++ src/authSlice.js
@@ -1,10 +1,10 @@
-import { createSlice } from '@reduxjs/toolkit';
 import { Cookies } from 'react-cookie';
+import { createSlice } from '@reduxjs/toolkit';
 
 const cookie = new Cookies();
 
 const initialState = {
-  isSignIn: cookie.get('token') !== undefined && cookie.get('token') !== "",
+  isSignIn: cookie.get('token') !== undefined && cookie.get('token') !== ''
 };
 
 export const authSlice = createSlice({
@@ -16,8 +16,8 @@ export const authSlice = createSlice({
     },
     signOut: (state) => {
       state.isSignIn = false;
-    },
-  },
+    }
+  }
 });
 
 export const { signIn, signOut } = authSlice.actions;
