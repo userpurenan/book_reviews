60593a6bd48f5193a91358ba80da61b3bc88c48b
userpurenan <nan691628@gmail.com>
2024-09-28 23:15:58 +0900

    テスト環境の設定を修正

--- backend/config/database.php
+++ backend/config/database.php
@@ -46,9 +46,9 @@ return [
         'testing'  => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'test_db'),
-            'port' => env('DB_PORT', '3307'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'host' => env('DB_HOST', 'db'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => 'test_database',
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
