74220f4ae71b28c3989736cf6fc927b868735436
userpurenan <nan691628@gmail.com>
2024-10-03 22:23:57 +0900

    画像アップロードの処理をサービス層に切り出した

--- backend/app/Http/Controllers/AuthUserController.php
+++ backend/app/Http/Controllers/AuthUserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Services\ImageUploadService;
+use App\Services\User\ImageUploadService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
